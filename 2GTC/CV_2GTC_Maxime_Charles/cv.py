98c6e02644b7c6e109d6beb708ed848a77aaf2c2
hgaron97-star <hgaron97@gmail.com>
2026-04-08 18:16:55 +0200

    Add files via upload

---
+++ 2GTC/CV_2GTC_Maxime_Charles/cv.py
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<title>Maxime Tigeon</title>
+<head>
+<meta charset="utf-8"/>
+<meta name="autor" content="Maxime Tigeon"/>
+<meta name="description" content="CV de Maxime"
+
+</head>
+<body>
+<p>Je suis actuellement en classe de seconde au lycée La
+Salle à Lille et à la recherche d’un stage d’observation
+dans le domaine du commerce.
+J’ai choisi ce secteur d’activité, car je suis intéressé par la
+vente, la comptabilitée</p>
+<ul>
+  <li><strong>Adresse : </strong>Tatooine, Galaxie lointaine, très très lointaine</li>
+  <li><strong>Téléphone : </strong>(06 18 23 57 88)</li>
+  <li><strong>email : </strong>maximetigeon@gmail.com</li>
+</ul>
+<h1>Diplomes et formation</h1>
+<p>Classe de seconde-Année 2025-2026 lyée La Salle-Lille Option : Sciences de l'ingénieur Brevet des collèges Collèges Saint-Joseph-Années 2021-2025</p>
+<h2>Stages</h2>
+<li>Stage de découverte en entreprise – Février 2026</li>
+<li>Intermarché Tourcoing</li>
+<li>Drive, mise en rayon, gestion des commandes</li>
+<li>stage en entreprise(contabilité)</li>=
+
+<h3></h3>
+<p></p>
+
+
